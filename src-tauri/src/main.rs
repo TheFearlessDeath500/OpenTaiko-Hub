4524a5b743382232a094a6265ee38cb3e0978f70
0auBSQ <58159635+0auBSQ@users.noreply.github.com>
2024-10-26 17:16:51 +0900

    hotfix: fix overwrighting invoke_handler

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -96,8 +96,7 @@ fn main() {
     .plugin(tauri_plugin_upload::init())
     .plugin(tauri_plugin_fs::init())
     .plugin(tauri_plugin_os::init())
-    .invoke_handler(tauri::generate_handler![unzip_and_get_first_folder])
-    .invoke_handler(tauri::generate_handler![execute_external_app])
+    .invoke_handler(tauri::generate_handler![unzip_and_get_first_folder, execute_external_app])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
